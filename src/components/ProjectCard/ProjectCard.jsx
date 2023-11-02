d962b355ac45ce41298c5c27d6cebcb5e0e437f1
Paige Godfrey <paagodfrey@gmail.com>
2023-11-01 19:37:04 -0700

    updated projectList and links

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -3,18 +3,28 @@ import './ProjectCard.css';
 
 export default function ProjectCard({ project }) {
 
-  let { title, description, technologies, img, link, github } = project;
+  let { title, description, technologies, image, link, github } = project;
 
   return (
     <div className="project-card">
-      <div className="image-container">
-        <img className="project-image" src={img}/>
+      <div className="project-card-header">
+        <img className="project-card-image" src={image} alt={image.split('.')[0] + ' image'} />
       </div>
-      <div className="card-body">
+      <div className="project-card-body">
         <h2>{title}</h2>
         <p>{description}</p>
         <p>{technologies}</p>
+        <div>
+          {link &&
+            <a href={link} target="_blank" rel="noreferrer" style={{marginRight: "5px"}}>
+              <button>Demo</button>
+            </a>}
+          {github &&
+            <a href={github} target="_blank" rel="noreferrer">
+              <button>Github</button>
+            </a>}
+        </div>
       </div>
-    </div>
+    </div >
   );
 }
--- src/projectList.js
+++ src/projectList.js
@@ -1,6 +1,8 @@
 import jobly from "./assets/images/jobly1.png"
 import microblog from "./assets/images/microblog1.png";
-import jumpOff from "./assets/images/jumpoff1.png";
+import squares from "./assets/images/squares.png";
+import kinfolx from "./assets/images/kinfolx.png";
+import shadowUI from "./assets/images/shadowUI.png";
 import flixster from "./assets/images/flixster1.png";
 
 let projects = [
@@ -13,9 +15,27 @@ let projects = [
     techDetail:`The frontend uses React Context to manage global state and the 
                 backend features a RESTful API with token-based authentication. 
                 Integration tests are written with Jest and SuperTest.`,
-    img: jobly,
+    image: jobly,
     link: "http://jobly-build.surge.sh/",
     github: "https://github.com/paigegodfrey/jobly"
+  },
+  {
+    title: "Kinfolx",
+    technologies: "React • JavaScript • CSS",
+    description: `Kinfolx is an Oakland-based coffee shop. Worked on a team
+                  of 4 to build improved React UI. Developed landing page and
+                  optimized performance via code splitting, image compression,
+                  and lazy loading.`,
+    image: kinfolx,
+    link: "https://kinfolx.onrender.com/",
+  },
+  {
+    title: "Shadow-UI",
+    technologies: "React • Storybook • CSS",
+    description: `Shadow-UI is a neumorphic design system focused on accessibility
+                  and ease of use. One of four contributors; work in progress.`,
+    image: shadowUI,
+    link: "https://www.shadow-ui.com/",
   },
   {
     title: "Microblog",
@@ -25,21 +45,17 @@ let projects = [
     techDetail:`Redux Thunk is used to execute asynchronous API calls, with blog 
                 titles and posts stored in separate data structures to optimize 
                 loading.`,
-    img: microblog,
-    link: "http://microblog.surge.sh/",
+    image: microblog,
     github: "https://github.com/paigegodfrey/microblog"
   },
   {
-    title: "JumpOffCampus",
-    technologies: "React • Ruby on Rails • PostgreSQL",
-    description: `JumpOffCampus enables students to find off-campus housing solutions in 
-                  their community.`,
-    techDetail:`During my internship, I worked primarily in React to refactor legacy 
-                components from classes to hooks and improve the apartment listings UI 
-                by separating list and map views and integrating Google Maps API. I also 
-                built and tested MVC components for “points of interest” feature, as part 
-                of an upgrade from Rails 2 to Rails 5.`,
-    img: jumpOff,
+    title: "Square Generator",
+    technologies: "React • JavaScript • CSS",
+    description: `Square Generator is a small React app that allows users to create, duplicate,
+                  and delete squares of prescribed colors and sizes.`,
+    image: squares,
+    link: "https://square-generator.netlify.app/",
+    github: "https://github.com/paigegodfrey/square-generator/tree/mvp"
   },
   {
     title: "Flixster",
@@ -49,7 +65,7 @@ let projects = [
                   genre, duration, rating, and description.`,
     techDetail:`Application is styled with pure CSS and features flexbox and grid. Data 
                 is sourced from The Movie Database API.`,
-    img: flixster,
+    image: flixster,
     link: "https://flixster.netlify.app/",
     github: "https://github.com/paigegodfrey/flixster"
   },
